359331b4bcc6454500e5772d17432169ce557633
Kat <k.augustyniakk@gmail.com>
2023-01-18 18:20:12 +0100

    Update Countries.js
    Update outdated REST API endpoint.

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -15,7 +15,7 @@ const Countries = () => {
     
 
     const fetchCountries = async () => {
-        return await fetch('https://restcountries.eu/rest/v2/all')
+        return await fetch('https://restcountries.com/v3.1/all')
           .then(res => res.json())
           .then(data => {
             setLoading(false);  
